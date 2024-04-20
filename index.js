9a24609353f9160263b5990ad9f5876f6a66c216
myraidevil <rohitm10123@gmail.com>
2024-04-20 17:50:29 +0530

    done

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    let low = 0;
+    let high = this.length;
 
-  get(pos) {}
+    while (low < high) {
+      const mid = Math.floor((low + high) / 2);
+      if (this.items[mid] < item) {
+        low = mid + 1;
+      } else {
+        high = mid;
+      }
+    }
 
-  max() {}
+    this.items.splice(low, 0, item);
+    this.length++;
+  }
 
-  min() {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  sum() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.length - 1];
+  }
 
-  avg() {}
+
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
 }
 
-module.exports = SortedList;
+sum() {
+  if (this.length === 0) {
+    return 0; 
+  }
+  return this.items.reduce((acc, curr) => acc + curr, 0); 
+}
+
+avg() {
+  if (this.length === 0) {
+    throw new Error('EmptySortedList'); 
+  }
+  return this.sum() / this.length; 
+}
+}
+
+export default SortedList;
